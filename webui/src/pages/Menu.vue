9469d51cefee6fae4bb63d541066873b6fc8bd08
nuch <126041764+handanuch@users.noreply.github.com>
2024-12-14 12:23:55 +0700

    Update Menu

--- webui/src/pages/Menu.vue
+++ webui/src/pages/Menu.vue
@@ -1,110 +1,435 @@
-<template>
-  <div class="container my-5 min-vh-100">
-    <h1 class="text-center mb-5">PRODUCTS</h1>
-    <div class="row">
-      <div class="col-md-8">
-        <div class="row">
-          <Products :filter="filter" />
+  <!-- @format -->
+
+  <template>
+    <nav class=" d-flex justify-content-between navbar navbar-expand-lg navbarbg">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-bold">
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">ITEM</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">HOT DRINK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">ICE DRINK</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">SMOOTHIES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-dark" href="#">FAST FOOD</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <div class="col-md-4" style="border-left: 1px solid #dee2e6">
-        <div class="sticky-top filter">
-          <div class="card">
-            <div class="card-body">
-              <h3 class="card-title mb-3">SEARCH BY</h3>
-              <div class="form-group">
-                <label for="type">Category:</label>
-                <select
-                  class="form-control mb-3"
-                  id="type"
-                  v-model="filter.type"
-                >
-                  <option value="">All</option>
-                  <option
-                    v-for="category in categories"
-                    :key="category"
-                    :value="category"
-                  >
-                    {{ category }}
-                  </option>
-                </select>
-              </div>
-              <div class="form-group mb-3">
-                <label for="name">Product Name:</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="name"
-                  placeholder="Enter product name..."
-                  v-model="filter.name"
-                />
-              </div>
-              <div class="row form-group mb-3">
-                <label for="price">Price range:</label>
-                <label class="col-md-12 text-center"
-                  >From ${{ formatNum(filter.priceMin) }} to ${{
-                    formatNum(filter.priceMax)
-                  }}</label
-                >
-                <input
-                  type="range"
-                  class="form-range"
-                  min="0"
-                  max="5000000"
-                  step="100000"
-                  v-model="filter.priceMax"
-                />
+    </nav>
+
+    <div class="container">
+      <div class="row">
+      
+        <div class="col-12 col-lg-8">
+          <section class="scroll">
+            <div class="text-center container">
+              <div class="row">
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+              
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div><div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div><div class="col-lg-4 col-md-12 mb-4">
+                  <div class="card img-hover">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt1wtkm9igXlN0tZ_lTRPRT4AFxjiYt9fnjA&s" class="w-100" />
+                    </div>
+                    <div class="card-body">
+                      <h5 class="card-title">ICE Latte</h5>
+                      <div class="star text-warning">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                      <h6 class="mt-2">$61.99</h6>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             </div>
+          </section>
+        </div>
+
+      
+        <div class="col-12 col-lg-4">
+          <div class="card-body card border-top border-bottom border-3">
+            <div class="row">
+              <div class="col-6">
+                <p class="lead fw-bold" style="color: #f37a27">Customer</p>
+              </div>
+            
+              <div class="col-6 text-end d-lg-none">
+                <button class="btn btn-outline-primary" type="button" data-bs-toggle="collapse" data-bs-target="#customerSummary" aria-expanded="false" aria-controls="customerSummary">
+                  View Summary
+                </button>
+              </div>
+            </div>
+
+            
+            <div class="collapse show d-lg-block" id="customerSummary">
+    <div class="row">
+      <div class="col-12">
+        <table class="table cafe-summery">
+          <thead class="table-light">
+            <tr class="main-hading">
+              <th>ITEM</th>
+              <th>PRICE</th>
+              <th style="width: 100px" class="text-center">QUANTITY</th>
+              <th class="text-center">TOTAL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+    <td colspan="4">
+      <div class="d-flex align-items-center justify-content-between">
+        <div class="item-name">1 Ice Latte</div>
+        <div class="item-price">10$</div>
+        <div class="quantity-control d-flex align-items-center" style="width: 100px;">
+          <button type="button" class="btn btn-sm btn-outline-danger btn-number" 
+            @click="updateQuantity(-1)" 
+            :disabled="amt <= min" 
+            style="padding: 0 6px; font-size: 12px;">
+            <i class="fas fa-minus"></i>
+          </button>
+          <input 
+            type="text" 
+            class="form-control text-center input-number mx-1" 
+            v-model="amt" 
+            style="padding: 0 6px; font-size: 12px;">
+
+          <button type="button" class="btn btn-sm btn-outline-primary btn-number" 
+            @click="updateQuantity(1)" 
+            style="padding: 0 6px; font-size: 12px;">
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
+        <div class="item-total text-center">200$</div>
+      </div>
+    </td>
+  </tr>
+
+          </tbody>
+        </table>
+
+        <div class="row">
+          <div class="col">
+            <p class="lead fw-bold" style="color: #f37a27">Order Summary</p>
+            <div class="row">
+              <div class="col">
+                <p>Grand Total</p>
+              </div>
+              <div class="col">
+                <p>320000$</p>
+              </div>
+              <hr />
+            </div>
+            <div class="row">
+              <div class="col">
+                <p>Discount</p>
+              </div>
+              <div class="col">
+                <p>320000$</p>
+              </div>
+              <hr />
+            </div>
+            <div class="row">
+              <div class="col">
+                <p>Total</p>
+              </div>
+              <div class="col">
+                <p>320000$</p>
+              </div>
+              <hr />
+            </div>
+            <!-- More summary rows here -->
+          </div>
+          
+          <div class="d-flex justify-content-between">
+            <button class="btn btn-primary"><i class="fas fa-plus"></i></button>
+            <button class="btn btn-primary">Pay</button>
+            <button class="btn btn-primary">Print</button>
           </div>
         </div>
       </div>
     </div>
   </div>
-</template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-import Products from "../components/Products.vue";
-// import { publicRequest } from "../requestMethod.js";
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
-// Reactive state for filter
-const filter = ref({
-  name: "",
-  type: "",
-  priceMin: 0,
-  priceMax: 1000000,
-  paginated: true,
-  itemsPerPage: 12,
-});
+  <script setup>
+  import { ref, onMounted } from "vue";
+  import Products from "../components/Products.vue";
 
-// Reactive state for categories
-const categories = ref([]);
+  const filter = ref({
+    name: "",
+    type: "",
+    priceMin: 0,
+    priceMax: 1000000,
+    paginated: true,
+    itemsPerPage: 12,
+  });
+  const categories = ref([]);
+  const amt = ref(1);
 
-// Fetch categories on component mounting
-onMounted(() => {
-  //   publicRequest
-  //     .get("/products/categories")
-  //     .then((res) => {
-  //       categories.value = res.data;
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     });
-});
+  const min = 1;
 
-// Method to format numbers
-const formatNum = (num) => {
-  return num.toLocaleString();
-};
-</script>
+  const updateQuantity = (amount) => {
+    const newValue = amt.value + amount;
 
-<style scoped>
-.filter {
-  top: 80px;
-}
+    if (newValue >= min) {
+      amt.value = newValue;
+    }
+  };
 
-.border-right {
-  border-right: 2px solid #89898935;
-}
-</style>
+  const onInput = (event) => {
+    const inputValue = Number(event.target.value);
+
+    if (inputValue >= min) {
+      amt.value = inputValue;
+    } else {
+      event.target.value = amt.value;
+    }
+  };
+  onMounted(() => {});
+
+  const formatNum = (num) => {
+    return num.toLocaleString();
+  };
+  </script>
+
+  <style>
+
+  .nav-link {
+    position: relative;
+    text-decoration: none;
+    color: #000;
+    transition: color 0.3s ease-in-out;
+  }
+
+  .nav-link:hover {
+    color: #007bff;
+  }
+
+  .nav-link::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: -2px;
+    width: 0;
+    height: 2px;
+    background-color: #007bff;
+    transition: width 0.3s ease-in-out;
+  }
+
+  .nav-link:hover::after {
+    width: 100%;
+  }
+
+  .navbar-nav {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  @media (min-width: 992px) {
+    .navbar-nav {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  /* For Wrap btn + -  */
+
+  .input-group {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .input-number {
+    text-align: center;
+    width: 68px;
+    height: 38px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+
+  .button {
+    display: flex;
+  }
+
+  .btn-number {
+    width: 40px;
+    height: 38px;
+  }
+
+  .scroll {
+
+    max-height: 100vh; 
+    overflow-y: scroll;
+    padding-bottom: 200px;
+  
+  }
+  .scroll {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+  }
+  .scroll::-webkit-scrollbar {
+    display: none;
+  } 
+  .card .bg-image {
+    transition: transform 0.3s ease;
+  }
+
+  .card:hover .bg-image {
+    transform: scale(0.95); 
+  }
+
+  </style>
